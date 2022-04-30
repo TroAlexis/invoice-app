e888fc7a09b513de3a78790d966b86c189658eda
Alex Troshin <a.troshin@idaproject.com>
2022-04-30 19:17:27 +0300

    feat(components): add ui paragraph component

---
+++ src/components/ui/Text/Text.tsx
@@ -0,0 +1,15 @@
+import { Constraints } from "../../../types/size";
+import { classNames } from "../../../assets/utils/dom";
+import { PropsWithChildren } from "react";
+
+import styles from "./Text.module.scss";
+
+interface Props {
+  type?: Constraints,
+}
+
+export default function Text({type = Constraints.TIGHT, children}: PropsWithChildren<Props>) {
+  const classes = classNames([styles.text, styles[type]]);
+
+  return <p className={classes}>{children}</p>
+}
