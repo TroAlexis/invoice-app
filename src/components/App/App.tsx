6888fb18cff125fa9a86ed1edf64a961c089636b
Alex Troshin <a.troshin@idaproject.com>
2022-05-02 22:44:06 +0300

    refactor(App): clear out root component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,19 +1,9 @@
 import React from "react";
 import "./App.module.scss";
-import Heading from "../ui/Heading/Heading";
-import Text from "../ui/Text/Text";
-import Container from "../ui/Container/Container";
+import Container from "components/ui/Container/Container";
 
 const App = (): JSX.Element => {
-  return (
-    <Container>
-      <Heading level="h1">
-        Hello fucking world!
-      </Heading>
-
-      <Text>Hello world!</Text>
-    </Container>
-  );
-}
+  return <Container>{/**/}</Container>;
+};
 
 export default App;
