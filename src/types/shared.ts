f3da688be433dee46a6698c5777a590fab449e19
Alex Troshin <a.troshin@idaproject.com>
2022-05-03 14:14:27 +0300

    feat(types): add shared slot props interface to reuse classes prop for components

--- src/types/shared.ts
+++ src/types/shared.ts
@@ -1,3 +1,7 @@
 export type ClassNamesObject = Record<string, boolean | undefined>;
 export type ClassNamesArray = Array<string | false | undefined>;
 export type ClassNames = string | ClassNamesObject | ClassNamesArray;
+
+export interface SlotProps {
+  classes: string;
+}
