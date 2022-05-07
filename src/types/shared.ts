eebb5ea0a058860075bd3c07dc9b4b79955999b3
Alex Troshin <a.troshin@idaproject.com>
2022-05-07 13:51:10 +0300

    feat(types): add WithForwardedRef shared interface

--- src/types/shared.ts
+++ src/types/shared.ts
@@ -1,7 +1,13 @@
+import React from "react";
+
 export type ClassNamesObject = Record<string, boolean | undefined>;
 export type ClassNamesArray = Array<string | false | undefined>;
 export type ClassNames = string | ClassNamesObject | ClassNamesArray;
 
 export interface SlotProps {
   classes: string;
+}
+
+export interface WithForwardedRef {
+  fRef?: React.Ref<HTMLElement> | null;
 }
