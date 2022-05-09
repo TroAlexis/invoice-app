40d571ea5d0bb5395beee2416d2ee6d73328dbaa
Alex Troshin <a.troshin@idaproject.com>
2022-05-09 13:32:22 +0300

    feat(types): add base and with props render slot types

--- src/types/shared.ts
+++ src/types/shared.ts
@@ -5,8 +5,17 @@ export type ClassNamesArray = Array<string | false | undefined>;
 export type ClassNames = string | ClassNamesObject | ClassNamesArray;
 
 export interface SlotProps {
-  classes: string;
+  classes?: string;
 }
+
+export type RenderSlot = (props?: SlotProps) => JSX.Element;
+
+export type RenderSlotProps<SlotName extends string, ParentProps> = SlotProps &
+  Omit<ParentProps, SlotName>;
+
+export type RenderSlotWithProps<SlotName extends string, ParentProps> = (
+  props: RenderSlotProps<SlotName, ParentProps>
+) => JSX.Element;
 
 export interface WithForwardedRef {
   fRef?: React.Ref<HTMLElement> | null;
