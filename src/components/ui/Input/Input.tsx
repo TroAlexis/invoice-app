27b4079d8e1052f1d3062344caf5fa1c0eea46aa
Alex Troshin <a.troshin@idaproject.com>
2022-05-09 13:33:27 +0300

    feat(Input): pass parent props into icon render slot

--- src/components/ui/Input/Input.tsx
+++ src/components/ui/Input/Input.tsx
@@ -5,25 +5,56 @@ import { classNames } from "assets/utils/dom";
 import withFormLabel, {
   WithFormLabelProps,
 } from "components/ui/hocs/withFormLabel/withFormLabel";
+import { RenderSlotProps, RenderSlotWithProps } from "@/types/shared";
 
 type InputProps = Omit<React.ComponentPropsWithRef<"input">, "size">;
 
-interface Props extends InputProps, WithFormLabelProps {
+interface ComponentProps {
+  inputClassName?: string;
   size?: Size.SMALL;
   type?: HTMLInputTypeAttribute;
+  icon?: RenderSlotWithProps<"icon", ComponentProps>;
 }
 
+interface Props extends ComponentProps, InputProps, WithFormLabelProps {}
+
 export const InputBase = forwardRef<HTMLInputElement, Props>(
-  ({ size = Size.SMALL, type = "text", className, ...inputProps }, ref) => {
+  (
+    {
+      size = Size.SMALL,
+      type = "text",
+      className,
+      inputClassName,
+      icon,
+      ...inputProps
+    },
+    ref
+  ) => {
     const wrapperClasses = classNames([className, styles.wrapper]);
-    const inputClasses = classNames([styles.input, size && styles[size]]);
+    const inputClasses = classNames([
+      inputClassName,
+      styles.input,
+      size && styles[size],
+      icon && styles["has-icon"],
+    ]);
 
     return (
       <div className={wrapperClasses}>
         <input type={type} className={inputClasses} ref={ref} {...inputProps} />
+
+        {renderInputIcon(icon, { size })}
       </div>
     );
   }
 );
+
+function renderInputIcon<P extends ComponentProps>(
+  icon: P["icon"],
+  props: RenderSlotProps<"icon", P>
+) {
+  const iconClasses = classNames([styles.icon]);
+
+  return icon ? icon({ ...props, classes: iconClasses }) : null;
+}
 
 export default withFormLabel(InputBase);
