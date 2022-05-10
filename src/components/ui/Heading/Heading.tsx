775c4a9d0f24d565568b49fee72919b669a01269
Alex Troshin <a.troshin@idaproject.com>
2022-05-10 15:01:03 +0300

    feat(Heading): allow passing component props through to heading element

--- src/components/ui/Heading/Heading.tsx
+++ src/components/ui/Heading/Heading.tsx
@@ -1,6 +1,6 @@
 import { Size } from "constants/size";
 import styles from "./Heading.module.scss";
-import React, { ElementType, PropsWithChildren } from "react";
+import React, { ComponentPropsWithoutRef, ElementType } from "react";
 import { classNames } from "utils/dom";
 
 const Headings = {
@@ -9,7 +9,7 @@ const Headings = {
   h3: "h3",
 } as const;
 
-interface Props {
+interface Props extends ComponentPropsWithoutRef<"p"> {
   element?: ElementType;
   level: keyof typeof Headings;
   size?: Size.SMALL;
@@ -19,15 +19,22 @@ export default function Heading({
   level = Headings.h1,
   element = Headings.h1,
   size,
+  className,
   children,
-}: PropsWithChildren<Props>) {
+  ...props
+}: Props) {
   const HeadingTag = element;
 
   const classes = classNames([
+    className,
     styles.heading,
     styles[level],
     size && styles[size],
   ]);
 
-  return <HeadingTag className={classes}>{children}</HeadingTag>;
+  return (
+    <HeadingTag {...props} className={classes}>
+      {children}
+    </HeadingTag>
+  );
 }
