184cac7a9205651a39f1281559ea9e258106b738
Alex Troshin <a.troshin@idaproject.com>
2022-05-21 15:03:51 +0300

    feat(Select): reduce margin top for outline variation

--- src/components/ui/Select/styles/menu.ts
+++ src/components/ui/Select/styles/menu.ts
@@ -1,9 +1,25 @@
-import { StylesCreator } from "components/ui/Select/styles/index";
+import {
+  PropertyCreator,
+  StylesCreator,
+} from "components/ui/Select/styles/index";
+import { CSSObjectWithLabel } from "react-select";
+
+type MenuPropertyCreator<P extends keyof CSSObjectWithLabel> = PropertyCreator<
+  P,
+  "menu"
+>;
+
+const getMarginTop: MenuPropertyCreator<"marginTop"> = ({ outline }) => {
+  if (outline) {
+    return ".8rem";
+  }
+  return "2.4rem";
+};
 
 const createMenuStyles: StylesCreator = (componentProps) => ({
-  menu: (base) => ({
+  menu: (base, props) => ({
     ...base,
-    marginTop: "2.4rem",
+    marginTop: getMarginTop(componentProps, props),
     boxShadow: "0px 10px 20px rgba(72, 84, 159, 0.25)",
     borderRadius: "8px",
   }),
