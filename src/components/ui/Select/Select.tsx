a39434efa00e77c5774d6c1cae25915ee9bbed52
Alex Troshin <a.troshin@idaproject.com>
2022-05-21 16:42:18 +0300

    feat(Select): add menu open state prop to react-select for styling based on state

--- src/components/ui/Select/Select.tsx
+++ src/components/ui/Select/Select.tsx
@@ -1,18 +1,45 @@
 import { composeStyles } from "components/ui/Select/styles";
+import { useState } from "react";
 import ReactSelect, { GroupBase, Props } from "react-select";
+import { ValueOf } from "types/shared";
+import { isUndefined } from "utils/common";
 
-export interface SelectProps {
-  outline?: boolean;
-}
+const MenuStates = {
+  OPEN: true,
+  CLOSED: false,
+} as const;
+
+type MenuState = ValueOf<typeof MenuStates>;
 
 export default function Select<
   Option,
   IsMulti extends boolean = false,
   Group extends GroupBase<Option> = GroupBase<Option>
->(props: Props<Option, IsMulti, Group> & SelectProps) {
-  const { outline, ...$props } = props;
-
+>(props: Props<Option, IsMulti, Group>) {
   const styles = composeStyles(props);
+  const { isOpen, openMenu, closeMenu } = useMenuState(
+    !!props.defaultMenuIsOpen
+  );
 
-  return <ReactSelect {...$props} styles={styles} blurInputOnSelect />;
+  const isMenuOpen = isUndefined(props.menuIsOpen) ? isOpen : props.menuIsOpen;
+
+  return (
+    <ReactSelect
+      {...props}
+      styles={styles}
+      blurInputOnSelect
+      menuIsOpen={isMenuOpen}
+      onMenuOpen={openMenu}
+      onMenuClose={closeMenu}
+    />
+  );
+}
+
+function useMenuState(initialState: MenuState) {
+  const [isOpen, setIsOpen] = useState<MenuState>(initialState);
+
+  const openMenu = () => setIsOpen(MenuStates.OPEN);
+  const closeMenu = () => setIsOpen(MenuStates.CLOSED);
+
+  return { isOpen, openMenu, closeMenu };
 }
