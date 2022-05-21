71df633245345c470d4be881c6da95069f803726
Alex Troshin <a.troshin@idaproject.com>
2022-05-21 14:58:37 +0300

    feat(Select): add outline styling for control component

--- src/components/ui/Select/styles/control.ts
+++ src/components/ui/Select/styles/control.ts
@@ -1,15 +1,49 @@
-import { StylesCreator, theme } from "components/ui/Select/styles/index";
+import {
+  PropertyCreator,
+  StylesCreator,
+  theme,
+} from "components/ui/Select/styles/index";
+import { CSSObjectWithLabel } from "react-select";
 import colors from "styles/exports/_colors.module.scss";
 
-const createControlStyles: StylesCreator = () => ({
-  control: (base, { isFocused }) => ({
+type ControlPropertyCreator<P extends keyof CSSObjectWithLabel> =
+  PropertyCreator<P, "control">;
+
+const getBorderColor: ControlPropertyCreator<"borderColor"> = (
+  { outline },
+  { isFocused }
+) => {
+  if (outline) {
+    return "transparent";
+  }
+  return isFocused ? theme.control.borderColor : colors["grey-300"];
+};
+
+const getHoverBorderColor: ControlPropertyCreator<"borderColor"> = ({
+  outline,
+}) => {
+  if (outline) {
+    return "transparent";
+  }
+  return theme.control.borderColor;
+};
+
+const getBoxShadow: ControlPropertyCreator<"boxShadow"> = ({ outline }) => {
+  if (outline) {
+    return "none";
+  }
+  return `0 0 0 1px ${theme.control.borderColor}`;
+};
+
+const createControlStyles: StylesCreator = (componentProps) => ({
+  control: (base, props) => ({
     ...base,
-    borderColor: isFocused ? theme.control.borderColor : colors["grey-300"],
-    boxShadow: isFocused
-      ? `0 0 0 1px ${theme.control.borderColor}`
+    borderColor: getBorderColor(componentProps, props),
+    boxShadow: props.isFocused
+      ? getBoxShadow(componentProps, props)
       : base.boxShadow,
     ":hover": {
-      ...theme.control,
+      borderColor: getHoverBorderColor(componentProps, props),
     },
   }),
 });
