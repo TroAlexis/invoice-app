e931a35a7ed2bca5cc1fe43b527ca365e93e6446
Alex Troshin <a.troshin@idaproject.com>
2022-05-21 16:31:11 +0300

    feat(declarations): add react-select declaration for custom props

--- declarations.d.ts
+++ declarations.d.ts
@@ -1,1 +1,13 @@
+import { GroupBase } from "react-select";
+
 declare module "*.svg?sprite";
+
+declare module "react-select/dist/declarations/src/Select" {
+  export interface Props<
+    Option,
+    IsMulti extends boolean,
+    Group extends GroupBase<Option>
+  > {
+    outline?: boolean;
+  }
+}
