b13dbc79834ab4b0a9825be3c11c43ec6abb960b
Alex Troshin <a.troshin@idaproject.com>
2022-05-26 17:20:00 +0300

    feat(store): add typed dispatch wrapper

--- src/hooks/useTypedStore.ts
+++ src/hooks/useTypedStore.ts
@@ -1,4 +1,6 @@
-import { TypedUseSelectorHook, useSelector } from "react-redux";
-import { RootState } from "@/store/reducers";
+import { AppDispatch, RootState } from "@/store";
+import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 
 export const useTypedSelector: TypedUseSelectorHook<RootState> = useSelector;
+
+export const useTypedDispatch = () => useDispatch<AppDispatch>();
