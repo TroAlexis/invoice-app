05e04e463e0ec072f01ba54cea71af34a36ac73a
Alex Troshin <a.troshin@idaproject.com>
2022-05-26 17:55:30 +0300

    feat(Container): add column prop to use as flex-column

--- src/components/ui/Container/Container.tsx
+++ src/components/ui/Container/Container.tsx
@@ -1,11 +1,17 @@
-import styles from "./Container.module.scss";
 import React, { ComponentPropsWithoutRef } from "react";
 import { classNames } from "utils/classnames";
+import styles from "./Container.module.scss";
 
-interface Props extends ComponentPropsWithoutRef<"div"> {}
+interface Props extends ComponentPropsWithoutRef<"div"> {
+  column?: boolean;
+}
 
-export default function Container({ children, className }: Props) {
-  const classes = classNames([styles.container, className]);
+export default function Container({ children, className, column }: Props) {
+  const classes = classNames([
+    styles.container,
+    className,
+    column && styles.column,
+  ]);
 
   return <div className={classes}>{children}</div>;
 }
