eac79a392e5b6574a58faea705eee0efee8bed55
Alex Troshin <a.troshin@idaproject.com>
2022-05-27 14:06:58 +0300

    feat(api): add mock api constants

--- src/assets/js/constants/api.ts
+++ src/assets/js/constants/api.ts
@@ -1,3 +1,6 @@
+export const MOCK_ON = !!process.env.REACT_APP_MOCK;
+
 export const API_URL = "https://faddkavehglvmtkkuizp.supabase.co";
+export const MOCK_API_URL = process.env.REACT_APP_MOCK_API_URL;
 export const API_KEY =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZhZGRrYXZlaGdsdm10a2t1aXpwIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTIxNzQ1MzAsImV4cCI6MTk2Nzc1MDUzMH0.qS_J3CRTJVmb0q_wPzUwOe7Uzoufnb0GZiMCC2AoxuU";
