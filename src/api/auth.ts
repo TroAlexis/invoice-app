0904684f623d30ab9409d3a8fbb3419a102d4130
Alex Troshin <a.troshin@idaproject.com>
2022-05-27 12:54:52 +0300

    refactor(auth): remove redundant variables, refactor into single module

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -2,44 +2,34 @@ import supabase from "@/api";
 import { UserCredentials } from "@supabase/supabase-js";
 import { AuthProvider } from "types/auth";
 
-const signUp = (...args: Parameters<AuthProvider["signUp"]>) => {
-  return supabase.auth.signUp(...args);
-};
-
-const logIn = (userCredentials: UserCredentials = {}) => {
-  return supabase.auth.signIn(userCredentials);
-};
-
-const logOut = () => {
-  return supabase.auth.signOut();
-};
-
-const getUser = () => {
-  return supabase.auth.user();
-};
-
-const getSession = () => {
-  return supabase.auth.session();
-};
-
-const refreshSession = () => {
-  return supabase.auth.refreshSession();
-};
-
-const onStateChange = (
-  ...args: Parameters<AuthProvider["onAuthStateChange"]>
-) => {
-  return supabase.auth.onAuthStateChange(...args);
-};
-
 const authApi = {
-  signUp,
-  logIn,
-  logOut,
-  getUser,
-  getSession,
-  refreshSession,
-  onStateChange,
-};
+  signUp: (...args: Parameters<AuthProvider["signUp"]>) => {
+    return supabase.auth.signUp(...args);
+  },
+
+  logIn: (userCredentials: UserCredentials = {}) => {
+    return supabase.auth.signIn(userCredentials);
+  },
+
+  logOut: () => {
+    return supabase.auth.signOut();
+  },
+
+  getUser: () => {
+    return supabase.auth.user();
+  },
+
+  getSession: () => {
+    return supabase.auth.session();
+  },
+
+  refreshSession: () => {
+    return supabase.auth.refreshSession();
+  },
+
+  onStateChange: (...args: Parameters<AuthProvider["onAuthStateChange"]>) => {
+    return supabase.auth.onAuthStateChange(...args);
+  },
+} as const;
 
 export default authApi;
