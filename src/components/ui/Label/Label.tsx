70ee8f51a39c4b29a14545e5d93e0bb4155db562
Alex Troshin <a.troshin@idaproject.com>
2022-05-29 01:46:44 +0300

    feat(Label): add status prop determining label color

--- src/components/ui/Label/Label.tsx
+++ src/components/ui/Label/Label.tsx
@@ -1,18 +1,28 @@
+import { Status } from "constants/invoices";
 import { ComponentPropsWithoutRef, ComponentType } from "react";
+import states from "styles/utils/states.module.scss";
 import { classNames } from "utils/classnames";
 import styles from "./Label.module.scss";
 
 interface Props extends ComponentPropsWithoutRef<"span"> {
   tag?: "span" | ComponentType;
+  status?: Status;
 }
+
+const statusClasses = {
+  [Status.DRAFT]: states.neutral,
+  [Status.PENDING]: states.pending,
+  [Status.PAID]: states["success-light"],
+};
 
 export default function Label({
   children,
   className,
   tag = "span",
+  status = Status.DRAFT,
   ...props
 }: Props) {
-  const classes = classNames([className, styles.label]);
+  const classes = classNames([className, styles.label, statusClasses[status]]);
   const Wrapper = tag;
 
   return (
