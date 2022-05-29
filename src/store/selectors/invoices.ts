4af7bec9bf33f4be2a9c2f7d26c4ab4428006d5a
Alex Troshin <a.troshin@idaproject.com>
2022-05-29 17:38:57 +0300

    feat(invoices): add filtered invoices selector

--- src/store/selectors/invoices.ts
+++ src/store/selectors/invoices.ts
@@ -1,3 +1,13 @@
 import { RootState } from "@/store";
+import { createSelector } from "reselect";
 
 export const invoicesSelector = (state: RootState) => state.invoices;
+
+export const filteredInvoicesSelector = createSelector(
+  invoicesSelector,
+  ({ items, filters }) => {
+    return items.filter((item) => {
+      return !filters.length || filters.some(({ filterer }) => filterer(item));
+    });
+  }
+);
