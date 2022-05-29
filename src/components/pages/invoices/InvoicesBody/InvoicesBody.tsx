d7f19e8f85bfe67f7fba4cc86bc289d17a2a872c
Alex Troshin <a.troshin@idaproject.com>
2022-05-29 17:47:50 +0300

    feat(InvoicesBody): add loader,transition between loading states, invoices list and invoices fetching

--- src/components/pages/invoices/InvoicesBody/InvoicesBody.tsx
+++ src/components/pages/invoices/InvoicesBody/InvoicesBody.tsx
@@ -1,23 +1,77 @@
-import { useTypedSelector } from "@/hooks/useTypedStore";
-import { invoicesSelector } from "@/store/selectors/invoices";
+import invoicesApi from "@/api/modules/invoices";
+import useEffectOnce from "@/hooks/useEffectOnce";
+import useLoading from "@/hooks/useLoading";
+import { useTypedDispatch, useTypedSelector } from "@/hooks/useTypedStore";
+import { filteredInvoicesSelector } from "@/store/selectors/invoices";
+import { InvoiceActionType } from "@/store/types/invoices";
 import InvoicesBodyEmpty from "components/pages/invoices/InvoicesBodyEmpty/InvoicesBodyEmpty";
+import InvoicesList from "components/pages/invoices/InvoicesList/InvoicesList";
 import Container from "components/ui/Container/Container";
+import Loader, { Speed } from "components/ui/Loader/Loader";
+import { Color } from "constants/color";
+import { FC, PropsWithChildren } from "react";
+import { CSSTransition, SwitchTransition } from "react-transition-group";
 import styles from "./InvoicesBody.module.scss";
 
-interface Props {}
-
 export default function InvoicesBody() {
-  const { items } = useTypedSelector(invoicesSelector);
+  const { invoices, loading } = useInvoices();
 
   return (
     <div className={styles.wrapper}>
       <Container column className={styles.container}>
-        {items.length ? (
-          "Invoices"
-        ) : (
-          <InvoicesBodyEmpty className={styles.empty} />
-        )}
+        <Transition loading={loading}>
+          {loading ? (
+            <Loader
+              className={styles.loader}
+              color={Color.VIOLET}
+              speed={Speed.SLOW}
+            />
+          ) : (
+            <Transition loading={!!invoices.length}>
+              {invoices.length ? (
+                <InvoicesList invoices={invoices} />
+              ) : (
+                <InvoicesBodyEmpty className={styles.empty} />
+              )}
+            </Transition>
+          )}
+        </Transition>
       </Container>
     </div>
   );
 }
+
+interface TransitionProps {
+  loading: boolean;
+}
+
+const Transition: FC<PropsWithChildren<TransitionProps>> = ({
+  loading,
+  children,
+}) => {
+  return (
+    <SwitchTransition mode="out-in">
+      <CSSTransition key={`${loading}`} classNames="fade" timeout={200}>
+        {children}
+      </CSSTransition>
+    </SwitchTransition>
+  );
+};
+
+const useInvoices = () => {
+  const invoices = useTypedSelector(filteredInvoicesSelector);
+  const { loading, withLoading } = useLoading(true);
+  const dispatch = useTypedDispatch();
+
+  const fetchInvoices = async () => {
+    const items = await invoicesApi.getAll();
+    dispatch({ type: InvoiceActionType.SET_ITEMS, items });
+  };
+  const fetchWithLoading = withLoading(fetchInvoices);
+
+  useEffectOnce(() => {
+    fetchWithLoading();
+  });
+
+  return { invoices, loading };
+};
