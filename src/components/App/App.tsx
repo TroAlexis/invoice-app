ae8b34713f0db7af43f2d6ccc08e353630ed9f56
Alex Troshin <a.troshin@idaproject.com>
2022-05-30 15:31:01 +0300

    feat(App): animate invoices routes

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,20 +1,33 @@
+import LayoutDefault from "@/layouts/default/default";
+import InvoicePage from "@/pages/invoices/_id/_id";
+import Invoices from "@/pages/invoices/index/invoices";
+import AnimatedRoutes from "components/AnimatedRoutes/AnimatedRoutes";
 import React from "react";
 import "react-datepicker/dist/react-datepicker.css";
 import { Navigate, Route, Routes } from "react-router-dom";
-import LayoutDefault from "@/layouts/default/default";
-import Invoices from "@/pages/invoices/index/invoices";
 
 const App = (): JSX.Element => {
   return (
     <Routes>
       <Route path="*" element={<LayoutDefault />}>
-        <Route index element={<Invoices />} />
-        <Route path="invoices" element={<Invoices />} />
+        <Route path="invoices/*" element={<InvoicesRoutes />} />
 
-        <Route path="*" element={<Navigate to="/" replace />} />
+        <Route path="*" element={<Navigate to="/invoices" replace />} />
       </Route>
     </Routes>
   );
 };
 
 export default App;
+
+const InvoicesRoutes = () => {
+  return (
+    <AnimatedRoutes
+      switchProps={{ mode: "out-in" }}
+      transitionProps={{ classNames: "fade", timeout: 200 }}
+    >
+      <Route index element={<Invoices />} />
+      <Route path=":id" element={<InvoicePage />} />
+    </AnimatedRoutes>
+  );
+};
