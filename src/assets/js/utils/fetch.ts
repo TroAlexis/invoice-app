195f619a9c630db0b3c015bd4293afa5dbc0134b
Alex Troshin <a.troshin@idaproject.com>
2022-05-30 15:45:48 +0300

    fix(utils): remove error handling in fetch wrapper

--- src/assets/js/utils/fetch.ts
+++ src/assets/js/utils/fetch.ts
@@ -1,17 +1,13 @@
 import { timeout } from "utils/common";
 
 export const $fetch = async (...args: Parameters<typeof fetch>) => {
-  try {
-    const response = await fetch(...args);
+  const response = await fetch(...args);
 
-    if (!response.ok) {
-      throw Error(response.statusText);
-    }
-
-    return response.json();
-  } catch (e) {
-    console.error(e);
+  if (!response.ok) {
+    throw Error(response.statusText);
   }
+
+  return response.json();
 };
 
 export const withTimeout = async <T>(
