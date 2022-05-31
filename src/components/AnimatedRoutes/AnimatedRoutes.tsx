e2bc8bfa484d54db260a07dfaf3d187ff4da7412
Alex Troshin <a.troshin@idaproject.com>
2022-06-01 00:47:01 +0300

    feat(AnimatedRoutes): add location prop for flexibility, e.g to use as modal background

--- src/components/AnimatedRoutes/AnimatedRoutes.tsx
+++ src/components/AnimatedRoutes/AnimatedRoutes.tsx
@@ -1,25 +1,28 @@
-import { SwitchTransitionProps } from "react-transition-group/SwitchTransition";
+import { Location, Routes, RoutesProps, useLocation } from "react-router-dom";
 import { CSSTransition, SwitchTransition } from "react-transition-group";
-import { Routes, RoutesProps, useLocation } from "react-router-dom";
 import { CSSTransitionProps } from "react-transition-group/CSSTransition";
+import { SwitchTransitionProps } from "react-transition-group/SwitchTransition";
 
 interface Props {
   switchProps: Omit<SwitchTransitionProps, "children">;
   transitionProps: CSSTransitionProps;
   children: RoutesProps["children"];
+  location?: Location;
 }
 
 export default function AnimatedRoutes({
   switchProps,
   transitionProps,
   children,
+  location,
 }: Props) {
-  const location = useLocation();
+  const routesLocation = useLocation();
+  const locationToUse = location || routesLocation;
 
   return (
     <SwitchTransition {...switchProps}>
-      <CSSTransition {...transitionProps} key={location.pathname}>
-        <Routes location={location}>{children}</Routes>
+      <CSSTransition {...transitionProps} key={locationToUse.pathname}>
+        <Routes location={locationToUse}>{children}</Routes>
       </CSSTransition>
     </SwitchTransition>
   );
