7827a64d45a94ed3410a1d5338bf6e677d3b0d52
Alex Troshin <a.troshin@idaproject.com>
2022-06-01 00:38:20 +0300

    feat(InvoiceButton): wrap with navlink opening invoice modal

--- src/components/elements/InvoiceButton/InvoiceButton.tsx
+++ src/components/elements/InvoiceButton/InvoiceButton.tsx
@@ -1,22 +1,32 @@
+import styles from "components/elements/InvoiceButton/InvoiceButton.module.scss";
 import Button from "components/ui/Button/Button";
 import Heading from "components/ui/Heading/Heading";
+import Icon from "components/ui/Icon/Icon";
 import { Size } from "constants/size";
 import React from "react";
+import { NavLink, useLocation } from "react-router-dom";
 import { SlotProps } from "types/shared";
-import styles from "components/elements/InvoiceButton/InvoiceButton.module.scss";
-import Icon from "components/ui/Icon/Icon";
 import { classNames } from "utils/classnames";
 
-interface Props
-  extends Omit<React.ComponentPropsWithoutRef<"button">, "color"> {}
+interface Props extends Omit<React.ComponentPropsWithoutRef<"a">, "color"> {}
 
-export default function InvoiceButton(attrs: Props) {
+export default function InvoiceButton({ className, ...props }: Props) {
+  const location = useLocation();
+  const classes = classNames([styles.link, className]);
+
   return (
-    <Button icon={renderButtonIcon} {...attrs}>
-      <Heading level="h3" element="p" size={Size.SMALL}>
-        New Invoice
-      </Heading>
-    </Button>
+    <NavLink
+      to="/invoices/new"
+      state={{ backgroundLocation: location }}
+      className={classes}
+      {...props}
+    >
+      <Button icon={renderButtonIcon}>
+        <Heading level="h3" element="p" size={Size.SMALL}>
+          New Invoice
+        </Heading>
+      </Button>
+    </NavLink>
   );
 }
 
