3862eddea36c6a68f175170d0ee7511ff274765d
Alex Troshin <a.troshin@idaproject.com>
2022-06-01 00:49:34 +0300

    feat(App): add routes with modal-aside layout for invoices

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,33 +1,69 @@
 import LayoutDefault from "@/layouts/default/default";
+import ModalAside from "@/layouts/modal-aside/modal-aside";
 import InvoicePage from "@/pages/invoices/_id/_id";
+import InvoicesNew from "@/pages/invoices/_id/new/new";
 import Invoices from "@/pages/invoices/index/invoices";
 import AnimatedRoutes from "components/AnimatedRoutes/AnimatedRoutes";
 import React from "react";
 import "react-datepicker/dist/react-datepicker.css";
-import { Navigate, Route, Routes } from "react-router-dom";
+import {
+  Location,
+  Navigate,
+  Route,
+  Routes,
+  useLocation,
+} from "react-router-dom";
 
 const App = (): JSX.Element => {
-  return (
-    <Routes>
-      <Route path="*" element={<LayoutDefault />}>
-        <Route path="invoices/*" element={<InvoicesRoutes />} />
+  const location = useLocation();
 
-        <Route path="*" element={<Navigate to="/invoices" replace />} />
-      </Route>
-    </Routes>
+  const state = location.state as { backgroundLocation?: Location };
+
+  const currentLocation = state?.backgroundLocation || location;
+
+  return (
+    <>
+      <Routes location={currentLocation}>
+        <Route path="*" element={<LayoutDefault />}>
+          <Route
+            path="invoices/*"
+            element={<InvoicesRoutes location={currentLocation} />}
+          />
+
+          <Route path="*" element={<Navigate to="/invoices" replace />} />
+        </Route>
+      </Routes>
+
+      {state?.backgroundLocation && <InvoicesModalRoutes />}
+    </>
   );
 };
 
 export default App;
 
-const InvoicesRoutes = () => {
+const InvoicesRoutes = ({ location }: { location: Location }) => {
   return (
     <AnimatedRoutes
+      location={location}
       switchProps={{ mode: "out-in" }}
       transitionProps={{ classNames: "fade", timeout: 200 }}
     >
       <Route index element={<Invoices />} />
       <Route path=":id" element={<InvoicePage />} />
+
+      <Route path="*" element={<Navigate to="/" replace />} />
     </AnimatedRoutes>
+  );
+};
+
+const InvoicesModalRoutes = () => {
+  return (
+    <Routes>
+      <Route path="invoices/*" element={<ModalAside />}>
+        <Route path="new" element={<InvoicesNew />} />
+
+        <Route path="*" element={<Navigate to="/invoices" replace />} />
+      </Route>
+    </Routes>
   );
 };
