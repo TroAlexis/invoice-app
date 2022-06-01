7b7948e3e4a99d30daf71e1894331eda34e6a025
Alex Troshin <a.troshin@idaproject.com>
2022-06-01 16:29:22 +0300

    feat(modal-aside): add navigating back on overlay click

--- src/layouts/modal-aside/modal-aside.tsx
+++ src/layouts/modal-aside/modal-aside.tsx
@@ -1,11 +1,23 @@
+import { useEventSelf } from "@/hooks/useEventSelf";
 import LayoutDefault from "@/layouts/default/default";
+import { useNavigate } from "react-router-dom";
 import styles from "./modal-aside.module.scss";
 
 export default function ModalAside() {
+  const { ref, withEventSelf } = useEventSelf();
+  const navigate = useNavigate();
+
+  const navigateBack = () => navigate(-1);
+  const handleClickSelf = withEventSelf(navigateBack);
+
   return (
     <LayoutDefault
       className={styles.modal}
-      mainClassName={styles.main}
+      mainProps={{
+        className: styles.main,
+        ref,
+        onClick: handleClickSelf,
+      }}
       sidebarClassName={styles.sidebar}
     />
   );
