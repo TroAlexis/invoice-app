db497224e07c7b8f3afdfe45ec4faa5e14909235
Alex Troshin <a.troshin@idaproject.com>
2022-06-05 15:54:17 +0300

    feat(Select): wrap select with label component

--- src/components/ui/Select/Select.tsx
+++ src/components/ui/Select/Select.tsx
@@ -1,13 +1,26 @@
+import withFormLabel, {
+  WithFormLabelProps,
+} from "components/ui/hocs/withFormLabel/withFormLabel";
 import Menu from "components/ui/Select/components/Menu";
 import { useMenuState } from "components/ui/Select/hooks/useMenuState";
 import { composeStyles } from "components/ui/Select/styles";
 import ReactSelect, { GroupBase, Props } from "react-select";
 
-export default function Select<
+type SelectProps<
   Option,
-  IsMulti extends boolean = false,
-  Group extends GroupBase<Option> = GroupBase<Option>
->(props: Props<Option, IsMulti, Group>) {
+  IsMulti extends boolean,
+  Group extends GroupBase<Option>
+> = WithFormLabelProps & Props<Option, IsMulti, Group>;
+
+type SelectWrapper = <
+  Option,
+  IsMulti extends boolean,
+  Group extends GroupBase<Option>
+>(
+  props: SelectProps<Option, IsMulti, Group>
+) => JSX.Element;
+
+const SelectBase: SelectWrapper = (props) => {
   const { styles, components, ...propsToSpread } = props;
   const composedStyles = composeStyles(props);
   const menuProps = useMenuState(props.menuIsOpen);
@@ -23,4 +36,12 @@ export default function Select<
       styles={composedStyles}
     />
   );
-}
+};
+
+const Select: SelectWrapper = (props) => {
+  const Component = withFormLabel<typeof props>(SelectBase);
+
+  return <Component {...props} />;
+};
+
+export default Select;
