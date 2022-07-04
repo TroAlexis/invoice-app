e0ecaedbf75e337b812642ac939de31381eeeec5
Alexey Troshin <aleksei.troshin@tedo.ru>
2022-07-04 16:52:41 +0300

    fix(api): remove invoices mock module to replace with supabase mocking

--- src/assets/js/constants/api.ts
+++ src/assets/js/constants/api.ts
@@ -1,5 +1,2 @@
-export const MOCK_ON = !!process.env.REACT_APP_MOCK;
-
 export const API_URL = process.env.REACT_APP_SUPABASE_API_URL as string;
-export const MOCK_API_URL = process.env.REACT_APP_MOCK_API_URL as string;
 export const API_KEY = process.env.REACT_APP_SUPABASE_KEY as string;
