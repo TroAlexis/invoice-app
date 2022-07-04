dce23795aa6742a1d18ea069e7aba1e9e1e7a9c1
Alexey Troshin <aleksei.troshin@tedo.ru>
2022-07-04 17:24:29 +0300

    fix(InvoiceModalCard): remove form tag

--- src/components/pages/invoices/_id/InvoiceModalCard/InvoiceModalCard.tsx
+++ src/components/pages/invoices/_id/InvoiceModalCard/InvoiceModalCard.tsx
@@ -9,5 +9,5 @@ export default function InvoiceModalCard({
 }: PropsOf<typeof Card>) {
   const classes = classNames([className, styles.card]);
 
-  return <Card className={classes} {...props} tag={"form"} />;
+  return <Card className={classes} {...props} />;
 }
