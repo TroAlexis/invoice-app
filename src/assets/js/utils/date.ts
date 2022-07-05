9cd87fc1dff35bc8b269357d9593a471200d342d
Alexey Troshin <aleksei.troshin@tedo.ru>
2022-07-05 23:05:34 +0300

    feat(date): add daysToMs utility

--- src/assets/js/utils/date.ts
+++ src/assets/js/utils/date.ts
@@ -1,3 +1,8 @@
+const HOURS_IN_DAY = 24;
+const MINUTES_IN_HOUR = 60;
+const SECONDS_IN_MINUTE = 60;
+const MS_IN_SECONDS = 1000;
+
 export const prettifyDate = (date: Date) => {
   return date.toLocaleDateString("en-US", {
     day: "2-digit",
@@ -12,4 +17,10 @@ export const isDate = (date: unknown): date is Date => {
 
 export const toUTCString = (date: Date): string => {
   return date.toUTCString();
+};
+
+export const daysToMs = (days: number): number => {
+  return (
+    days / HOURS_IN_DAY / MINUTES_IN_HOUR / SECONDS_IN_MINUTE / MS_IN_SECONDS
+  );
 };
