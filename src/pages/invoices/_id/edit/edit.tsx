90dde733be6944f90d2f0f9e80a1bd69d8426f63
Alexey Troshin <aleksei.troshin@tedo.ru>
2022-07-05 23:47:49 +0300

    feat(pages): add invoice editing and adding modal forms

--- src/pages/invoices/_id/edit/edit.tsx
+++ src/pages/invoices/_id/edit/edit.tsx
@@ -1,11 +1,36 @@
+import invoicesApi from "@/api/modules/invoices/invoices";
+import { apiInvoiceShaper } from "@/api/modules/invoices/invoices.shaper";
+import { useInvoiceForm } from "@/hooks/useInvoiceForm";
+import useLoading from "@/hooks/useLoading";
 import { useInvoice } from "@/pages/invoices/_id/_id";
 import InvoiceModalCard from "components/pages/invoices/_id/InvoiceModalCard/InvoiceModalCard";
 import InvoiceForm from "components/pages/invoices/InvoiceForm/InvoiceForm";
 import InvoiceId from "components/pages/invoices/InvoiceId/InvoiceId";
+import Button from "components/ui/Button/Button";
+import { Color } from "constants/color";
+import { FormEvent } from "react";
+import { useNavigate } from "react-router-dom";
+import { withEventPrevent } from "utils/events";
 import styles from "./edit.module.scss";
 
 export default function InvoiceEdit() {
   const { invoice } = useInvoice();
+  const { handleChange, handleInput, state } = useInvoiceForm(invoice);
+  const { loading, withLoading } = useLoading();
+
+  const navigate = useNavigate();
+  const goBack = () => navigate(-1);
+
+  const handleSave = withLoading(() => {
+    if (invoice?.id) {
+      const data = apiInvoiceShaper(state);
+      return invoicesApi.patch(invoice.id, data);
+    }
+  });
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    return withEventPrevent(e, handleSave);
+  };
 
   return (
     <InvoiceModalCard>
@@ -18,8 +43,23 @@ export default function InvoiceEdit() {
             </InvoiceId>
           </>
         )}
-        invoice={invoice}
-      />
+        onSubmit={handleSubmit}
+        onChange={handleChange}
+        onInput={handleInput}
+        invoice={state}
+      >
+        <Button
+          color={Color.SECONDARY}
+          className={styles.cancel}
+          onClick={goBack}
+        >
+          Cancel
+        </Button>
+
+        <Button color={Color.VIOLET} type="submit" loading={loading}>
+          Save Changes
+        </Button>
+      </InvoiceForm>
     </InvoiceModalCard>
   );
 }
--- src/pages/invoices/_id/new/new.tsx
+++ src/pages/invoices/_id/new/new.tsx
@@ -1,12 +1,71 @@
+import { invoicesApi } from "@/api/modules/invoices/invoices";
+import { apiInvoiceShaper } from "@/api/modules/invoices/invoices.shaper";
+import { useInvoiceForm } from "@/hooks/useInvoiceForm";
+import useLoading from "@/hooks/useLoading";
 import InvoiceModalCard from "components/pages/invoices/_id/InvoiceModalCard/InvoiceModalCard";
 import InvoiceForm from "components/pages/invoices/InvoiceForm/InvoiceForm";
+import Button from "components/ui/Button/Button";
+import { Color } from "constants/color";
+import { Status } from "constants/invoices";
+import { FormEvent } from "react";
+import { useNavigate } from "react-router-dom";
+import { withEventPrevent } from "utils/events";
+import classes from "./new.module.scss";
 
 interface Props {}
 
 export default function InvoicesNew() {
+  const { handleChange, handleInput, state } = useInvoiceForm();
+  const { loading, withLoading } = useLoading();
+  const saveAsDraft = withLoading(() => {
+    const invoice = apiInvoiceShaper(state);
+    return invoicesApi.add({
+      ...invoice,
+      status: Status.DRAFT,
+    });
+  });
+
+  const handleSave = withLoading(() => {
+    const invoice = apiInvoiceShaper(state);
+    return invoicesApi.add(invoice);
+  });
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    return withEventPrevent(e, handleSave);
+  };
+
+  const navigate = useNavigate();
+  const goBack = () => navigate(-1);
+
   return (
     <InvoiceModalCard>
-      <InvoiceForm heading="New Invoice" />
+      <InvoiceForm
+        heading="New Invoice"
+        onChange={handleChange}
+        onInput={handleInput}
+        invoice={state}
+        onSubmit={handleSubmit}
+      >
+        <Button
+          onClick={goBack}
+          color={Color.SECONDARY}
+          className={classes.discard}
+        >
+          Discard
+        </Button>
+
+        <Button
+          onClick={saveAsDraft}
+          color={Color.GREY}
+          className={classes.draft}
+          loading={loading}
+        >
+          Save as Draft
+        </Button>
+        <Button color={Color.VIOLET} type="submit" loading={loading}>
+          Save & Send
+        </Button>
+      </InvoiceForm>
     </InvoiceModalCard>
   );
 }
