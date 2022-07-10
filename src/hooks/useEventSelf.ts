077880167d01c0592cc0aff5a0a013227b6900b4
Alexey Troshin <aleksei.troshin@tedo.ru>
2022-07-10 16:29:44 +0300

    fix(useEventSelf): prevent withEventSelf being recreated on every render

--- src/hooks/useEventSelf.ts
+++ src/hooks/useEventSelf.ts
@@ -1,17 +1,18 @@
-import { EventHandler, SyntheticEvent, useRef } from "react";
+import { EventHandler, SyntheticEvent, useCallback, useRef } from "react";
 
 export const useEventSelf = <E extends HTMLElement>() => {
   const ref = useRef<E>(null);
 
-  const withEventSelf = <C extends EventHandler<SyntheticEvent<E>>>(
-    callback: C
-  ) => {
-    return (event: SyntheticEvent<E>) => {
-      if (event.target === ref.current) {
-        callback(event);
-      }
-    };
-  };
+  const withEventSelf = useCallback(
+    <C extends EventHandler<SyntheticEvent<E>>>(callback: C) => {
+      return (event: SyntheticEvent<E>) => {
+        if (event.target === ref.current) {
+          callback(event);
+        }
+      };
+    },
+    []
+  );
 
   return { ref, withEventSelf };
 };
