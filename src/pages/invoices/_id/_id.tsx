cf9518a84ecb14e57c8c02fa459a14b601170a72
Alexey Troshin <aleksei.troshin@tedo.ru>
2022-07-12 14:09:53 +0300

    fix(invoices): add loader for invoice id page

--- src/pages/invoices/_id/_id.tsx
+++ src/pages/invoices/_id/_id.tsx
@@ -1,17 +1,23 @@
-import { useTypedSelector } from "@/hooks/useTypedStore";
+import useEffectOnce from "@/hooks/useEffectOnce";
+import useLoading from "@/hooks/useLoading";
+import { useTypedDispatch, useTypedSelector } from "@/hooks/useTypedStore";
+import { fetchInvoices } from "@/store/action-creators/invoices";
 import { invoicesSelector } from "@/store/selectors/invoices";
 import InvoicesIdBackButton from "components/pages/invoices/_id/InvoicesIdBackButton/InvoicesIdBackButton";
 import InvoicesIdDetail from "components/pages/invoices/_id/InvoicesIdDetail/InvoicesIdDetail";
 import InvoicesIdHeader from "components/pages/invoices/_id/InvoicesIdHeader/InvoicesIdHeader";
 import InvoicesPageSection from "components/pages/invoices/InvoicesPageSection/InvoicesPageSection";
 import Container from "components/ui/Container/Container";
+import Loader from "components/ui/Loader/Loader";
+import { Color } from "constants/color";
 import { NavLink, useParams } from "react-router-dom";
 import styles from "./_id.module.scss";
 
 export default function InvoicePage() {
-  const { invoice } = useInvoice();
+  const { invoice, loading } = useInvoice();
   return (
     <InvoicesPageSection>
+      {loading && <Loader className={styles.loader} color={Color.VIOLET} />}
       {invoice && (
         <Container>
           <NavLink to="/invoices" className={styles["back-link"]}>
@@ -37,6 +43,15 @@ type InvoiceParams = {
 
 export function useInvoice() {
   const { items } = useTypedSelector(invoicesSelector);
+  const dispatch = useTypedDispatch();
+  const { loading, withLoading } = useLoading();
+  const fetchWithLoading = withLoading(fetchInvoices);
+
+  useEffectOnce(() => {
+    if (!items.length) {
+      fetchWithLoading(dispatch);
+    }
+  });
 
   const { id: invoiceId } = useParams<InvoiceParams>();
 
@@ -44,5 +59,5 @@ export function useInvoice() {
     return Number(invoiceId) === id;
   });
 
-  return { invoice };
+  return { invoice, loading };
 }
