c16abe3013a6fa44689a31d66bd22b20c5df9b62
Alexey Troshin <aleksei.troshin@tedo.ru>
2022-07-12 14:10:25 +0300

    feat: subscribe to invoices changes and refetch on change

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,3 +1,5 @@
+import supabase from "@/api";
+import { fetchInvoices } from "@/store/action-creators/invoices";
 import rootReducer from "@/store/reducers";
 import { applyMiddleware, legacy_createStore as createStore } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
@@ -9,6 +11,13 @@ const compose = composeWithDevTools({ trace: true });
 const composedEnhancers = compose(middlewareEnhancer);
 
 const store = createStore(rootReducer, undefined, composedEnhancers);
+
+supabase
+  .from("invoices")
+  .on("*", () => {
+    return fetchInvoices(store.dispatch);
+  })
+  .subscribe();
 
 export default store;
 
